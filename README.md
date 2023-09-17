f55a2d5914ce1663bb7156b4f59b197479245bc5
lighttigerXIV <afonsofonseca14401@gmail.com>
2023-09-17 17:38:11 +0100

    added support to windows

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Session Manager Extension
+# Bookmarks
 ### About
-This extension adds an option to Shutdown, Reboot, Suspend, Hibernate and Logout.
+This extension lets you open saved urls right from the launcher, removing the need to click on the browser bookmarks bar.
+![image](https://github.com/lighttigerXIV/simple-kl-bookmarks-extension/assets/35658492/aae8ca7d-ffa9-4a57-970f-56ae56de991a)
 
-### Preview
-![image](https://github.com/lighttigerXIV/simple-kl-session-manager-extension/assets/35658492/a01c146c-1bb2-48fe-8d07-b16d25ce0d02)
 
+### How To Use
+#### Adding a bookmark
+`{keyword} add {url} as {name}`
+
+![image](https://github.com/lighttigerXIV/simple-kl-bookmarks-extension/assets/35658492/bcfb9aa7-b049-42d4-a74e-aaf896c2e5a2)
+
+#### Remove a bookmark
+`{keyword} remove {name or url}`
+
+![image](https://github.com/lighttigerXIV/simple-kl-bookmarks-extension/assets/35658492/ec42cd4c-6567-4577-be42-60d2c503c207)
+
+#### Opening a bookmark
+`{keyword} {name or url}`
+
+![image](https://github.com/lighttigerXIV/simple-kl-bookmarks-extension/assets/35658492/4d48ddd2-3eec-4d10-96cd-84f95bd3d012)
+
+#### Editing bookmarks
+`{keyword} edit`
+
+![image](https://github.com/lighttigerXIV/simple-kl-bookmarks-extension/assets/35658492/53e9a4d4-e37e-4ef9-8513-58e5a08cc7f3)
+
+![image](https://github.com/lighttigerXIV/simple-kl-bookmarks-extension/assets/35658492/9a0ca1e0-c232-41cd-a294-d2113ce18595)
--- src/main.rs
+++ src/main.rs
@@ -1,155 +1,244 @@
-use std::process::Command;
+//Add this to hide commands on windows
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::{os::windows::process::CommandExt, process::Command};
 
 use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
+use resources::{get_logout_icon, get_reboot_icon, get_shutdown_icon, get_zzz_icon, EXTENSION_ID, get_lock_icon};
 use simple_kl_rs::{
     actions::{ExtensionAction, ResultAction},
-    extensions::get_parameters,
-    extensions::Function::GetResults,
-    extensions::{emit_results, Function::RunAction},
-    paths::get_extension_icon,
+    extensions::{emit_results, get_parameters, Function},
+    others::{FLAG_NO_WINDOW},
     results::{IconWithTextResult, SimpleKLResult},
-    settings::Settings,
 };
+use simple_kl_rs::settings::Settings;
+
+pub mod resources;
 
 fn main() {
-    let parameters = get_parameters().unwrap();
+    let parameters = get_parameters().expect("Error getting parameters");
     let function = parameters.function;
-    let extension_id = "com-lighttigerxiv-session-manager";
 
     match function {
-        GetResults => {
-            let search_text = parameters.search_text.unwrap();
-            let matcher = SkimMatcherV2::default();
+        Function::GetResults => {
             let mut results: Vec<SimpleKLResult> = Vec::new();
+            let search_text = &parameters.search_text.unwrap();
+            let fuzzy_matcher = SkimMatcherV2::default();
 
-            if matcher.fuzzy_match("shutdown", &search_text).is_some() {
-                results.push(SimpleKLResult::IconWithText(
-                    IconWithTextResult::new_with_color(
-                        get_extension_icon(extension_id, "@src/images/shutdown.svg").unwrap(),
-                        "accent",
-                        "Shutdown",
-                        ResultAction::ExtensionAction(ExtensionAction::new(
-                            extension_id,
-                            "shutdown",
-                        )),
-                    ),
-                ))
+            if fuzzy_matcher.fuzzy_match("shutdown", search_text).is_some() {
+                let result = SimpleKLResult::IconWithText(IconWithTextResult::new_with_color(
+                    get_shutdown_icon().unwrap(),
+                    "Shutdown",
+                    ResultAction::ExtensionAction(ExtensionAction::new(EXTENSION_ID, "shutdown")),
+                ));
+
+                results.push(result);
             }
 
-            if matcher.fuzzy_match("reboot", &search_text).is_some() {
-                results.push(SimpleKLResult::IconWithText(
-                    IconWithTextResult::new_with_color(
-                        get_extension_icon(extension_id, "@src/images/reboot.svg").unwrap(),
-                        "accent",
-                        "Reboot",
-                        ResultAction::ExtensionAction(ExtensionAction::new(extension_id, "reboot")),
-                    ),
-                ))
+            if fuzzy_matcher.fuzzy_match("reboot", search_text).is_some() {
+                let result = SimpleKLResult::IconWithText(IconWithTextResult::new_with_color(
+                    get_reboot_icon().unwrap(),
+                    "Reboot",
+                    ResultAction::ExtensionAction(ExtensionAction::new(EXTENSION_ID, "reboot")),
+                ));
+
+                results.push(result);
             }
 
-            if matcher.fuzzy_match("suspend", &search_text).is_some() {
-                results.push(SimpleKLResult::IconWithText(
-                    IconWithTextResult::new_with_color(
-                        get_extension_icon(extension_id, "@src/images/zzz.svg").unwrap(),
-                        "accent",
-                        "Suspend",
-                        ResultAction::ExtensionAction(ExtensionAction::new(
-                            extension_id,
-                            "suspend",
-                        )),
-                    ),
-                ))
+            if fuzzy_matcher.fuzzy_match("suspend", search_text).is_some() {
+                let result = SimpleKLResult::IconWithText(IconWithTextResult::new_with_color(
+                    get_zzz_icon().unwrap(),
+                    "Suspend",
+                    ResultAction::ExtensionAction(ExtensionAction::new(EXTENSION_ID, "suspend")),
+                ));
+
+                results.push(result);
             }
 
-            if matcher.fuzzy_match("hibernate", &search_text).is_some() {
-                results.push(SimpleKLResult::IconWithText(
-                    IconWithTextResult::new_with_color(
-                        get_extension_icon(extension_id, "@src/images/zzz.svg").unwrap(),
-                        "accent",
+            if cfg!(target_os = "linux") {
+                if fuzzy_matcher
+                    .fuzzy_match("hibernate", search_text)
+                    .is_some()
+                {
+                    let result = SimpleKLResult::IconWithText(IconWithTextResult::new_with_color(
+                        get_zzz_icon().unwrap(),
                         "Hibernate",
                         ResultAction::ExtensionAction(ExtensionAction::new(
-                            extension_id,
+                            EXTENSION_ID,
                             "hibernate",
                         )),
-                    ),
-                ))
+                    ));
+
+                    results.push(result);
+                }
             }
 
-            if matcher.fuzzy_match("logout", &search_text).is_some() {
-                results.push(SimpleKLResult::IconWithText(
-                    IconWithTextResult::new_with_color(
-                        get_extension_icon(extension_id, "@src/images/logout.svg").unwrap(),
-                        "accent",
-                        "Logout",
-                        ResultAction::ExtensionAction(ExtensionAction::new(extension_id, "logout")),
-                    ),
-                ))
+            if fuzzy_matcher.fuzzy_match("logout", search_text).is_some() {
+                let result = SimpleKLResult::IconWithText(IconWithTextResult::new_with_color(
+                    get_logout_icon().unwrap(),
+                    "Logout",
+                    ResultAction::ExtensionAction(ExtensionAction::new(EXTENSION_ID, "logout")),
+                ));
+
+                results.push(result);
             }
 
-            emit_results(results);
+            if fuzzy_matcher.fuzzy_match("lock", search_text).is_some() {
+                let result = SimpleKLResult::IconWithText(IconWithTextResult::new_with_color(
+                    get_lock_icon().unwrap(),
+                    "Lock",
+                    ResultAction::ExtensionAction(ExtensionAction::new(EXTENSION_ID, "lock")),
+                ));
+
+                results.push(result);
+            }
+
+            emit_results(&results);
         }
-        RunAction => match parameters.action.unwrap().as_str() {
-            "shutdown" => {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg("systemctl poweroff")
-                    .output()
-                    .expect("Error running poweroff command");
-            }
-            "reboot" => {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg("systemctl reboot")
-                    .output()
-                    .expect("Error running reboot command");
-            }
-            "suspend" => {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg("systemctl suspend")
-                    .output()
-                    .expect("Error running suspend command");
-            }
-            "hibernate" => {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg("systemctl hibernate")
-                    .output()
-                    .expect("Error running shutdown command");
-            }
-            "logout" => {
-                let desktop_environment_setting =
-                    Settings::get_extension_setting(extension_id, "desktop_environment").unwrap();
+        Function::RunAction => {
+            let action = &parameters.action.unwrap();
 
-                match desktop_environment_setting.as_str() {
-                    "gnome" => {
+            if cfg!(target_os = "windows") {
+
+                if action == "shutdown" {
+                    Command::new("powershell")
+                        .arg("-Command")
+                        .arg("Stop-Computer")
+                        .creation_flags(FLAG_NO_WINDOW)
+                        .output()
+                        .expect("Error running shutdown command");
+                }
+
+                if action == "reboot" {
+                    Command::new("powershell")
+                        .arg("-Command")
+                        .arg("Restart-Computer")
+                        .creation_flags(FLAG_NO_WINDOW)
+                        .output()
+                        .expect("Error running reboot command");
+                }
+
+                if action == "suspend" {
+                    Command::new("Rundll32.exe")
+                        .arg("powrprof.dll,SetSuspendState")
+                        .arg("Sleep")
+                        .creation_flags(FLAG_NO_WINDOW)
+                        .output()
+                        .expect("Error running suspend command");
+                }
+
+                if action == "logout" {
+                    Command::new("shutdown.exe")
+                        .arg("-l")
+                        .creation_flags(FLAG_NO_WINDOW)
+                        .output()
+                        .expect("Error running logout command");
+                }
+
+                if action == "lock"{
+                    Command::new("Rundll32.exe")
+                        .arg("user32.dll,LockWorkStation")
+                        .creation_flags(FLAG_NO_WINDOW)
+                        .output()
+                        .expect("Error running lock command");
+                }
+            } else {
+                if action == "shutdown" {
+                    Command::new("sh")
+                        .arg("-c")
+                        .arg("systemctl poweroff")
+                        .output()
+                        .expect("Error running poweroff command");
+                }
+
+                if action == "reboot" {
+                    Command::new("sh")
+                        .arg("-c")
+                        .arg("systemctl reboot")
+                        .output()
+                        .expect("Error running reboot command");
+                }
+
+                if action == "suspend" {
+                    Command::new("sh")
+                        .arg("-c")
+                        .arg("systemctl suspend")
+                        .output()
+                        .expect("Error running suspend command");
+                }
+
+                if action == "hibernate"{
+                    Command::new("sh")
+                        .arg("-c")
+                        .arg("systemctl hibernate")
+                        .output()
+                        .expect("Error running shutdown command");
+                }
+
+                if action == "logout" {
+                    let de_setting =
+                        &Settings::get_extension_setting(EXTENSION_ID, "desktop_environment").unwrap();
+
+                    if de_setting == "gnome"{
                         Command::new("sh")
                             .arg("-c")
                             .arg("gnome-session-quit --no-prompt")
                             .output()
                             .expect("Error running logout command");
                     }
-                    "kde" => {
+
+                    if de_setting == "kde"{
                         Command::new("sh")
                             .arg("-c")
                             .arg("qdbus org.kde.ksmserver /KSMServer logout 0 0 2")
                             .output()
                             .expect("Error running logout command");
                     }
-                    "custom" => {
-                        let custom_logout_setting =
-                            Settings::get_extension_setting(extension_id, "custom_logout").unwrap();
+
+                    if de_setting == "custom"{
+                        let logout_setting =
+                            &Settings::get_extension_setting(EXTENSION_ID, "custom_logout").unwrap();
 
                         Command::new("sh")
                             .arg("-c")
-                            .arg(custom_logout_setting)
+                            .arg(logout_setting)
                             .output()
                             .expect("Error running logout command");
                     }
-                    _ => {}
-                };
+                }
+
+                if action == "lock"{
+                    let de_setting =
+                        &Settings::get_extension_setting(EXTENSION_ID, "desktop_environment").unwrap();
+
+                    if de_setting == "gnome"{
+                        Command::new("sh")
+                            .arg("-c")
+                            .arg("dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock")
+                            .output()
+                            .expect("Error running logout command");
+                    }
+
+                    if de_setting == "kde"{
+                        Command::new("sh")
+                            .arg("-c")
+                            .arg("loginctl lock-session")
+                            .output()
+                            .expect("Error running logout command");
+                    }
+
+                    if de_setting == "custom"{
+                        let lock_setting =
+                            &Settings::get_extension_setting(EXTENSION_ID, "custom_lock").unwrap();
+
+                        Command::new("sh")
+                            .arg("-c")
+                            .arg(lock_setting)
+                            .output()
+                            .expect("Error running logout command");
+                    }
+                }
             }
-            _ => {}
-        },
+        }
     }
 }
