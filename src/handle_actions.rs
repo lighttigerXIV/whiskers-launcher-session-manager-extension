fe039862d3f61588ec050f7c295435e024778669
lighttigerxiv <lighttigerxiv@gmail.com>
2024-06-19 18:50:54 +0100

    add windows extension

--- src/handle_actions.rs
+++ src/handle_actions.rs
@@ -1,19 +1,19 @@
 use std::process::Command;
-use whiskers_launcher_rs::api::extensions::{get_extension_setting, ExtensionRequest};
+use whiskers_launcher_rs::api::extensions::ExtensionRequest;
 
-use crate::EXTENSION_ID;
+#[cfg(target_os = "linux")]
+use {crate::EXTENSION_ID, whiskers_launcher_rs::api::extensions::get_extension_setting};
 
 #[cfg(target_os = "windows")]
-use {std::os::windows::process::CommandExt, whiskers_launcher_rs::others::FLAG_NO_WINDOW};
+use {std::os::windows::process::CommandExt, whiskers_launcher_rs::utils::FLAG_NO_WINDOW};
 
 pub fn handle_actions(request: ExtensionRequest) {
     let action = request.extension_action.unwrap();
-    let preset = get_extension_setting(EXTENSION_ID, "preset").unwrap();
-    let use_custom_preset = preset == "custom";
 
     #[cfg(target_os = "windows")]
-    if cfg!(target_os = "windows") {
-        if action == "shut_down" {
+    {
+
+        if action == "shutdown" {
             Command::new("powershell")
                 .arg("-Command")
                 .arg("Stop-Computer")
@@ -22,7 +22,7 @@ pub fn handle_actions(request: ExtensionRequest) {
                 .expect("Error running shutdown command");
         }
 
-        if action == "reboot" {
+        if action == "restart" {
             Command::new("powershell")
                 .arg("-Command")
                 .arg("Restart-Computer")
@@ -60,6 +60,8 @@ pub fn handle_actions(request: ExtensionRequest) {
     #[cfg(target_os = "linux")]
     if cfg!(target_os = "linux") {
         let mut command = String::from("");
+        let preset = get_extension_setting(EXTENSION_ID, "preset").unwrap();
+        let use_custom_preset = preset == "custom";
 
         if action == "shutdown" {
             command = if use_custom_preset {
